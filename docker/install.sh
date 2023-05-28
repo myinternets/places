4b6d2746a6041ebe5f1c20624c73986e7d0a16a5
Tarek ZIADE <tarek@ziade.org>
2023-05-27 22:39:06 -0700

    remove cruft

--- docker/install.sh
+++ docker/install.sh
@@ -2,22 +2,6 @@
 set -exu
 set -o pipefail
 
-#apt-get update
-#apt-get install --no-install-recommends -y \
-#		software-properties-common \
-#		zlib1g-dev \
-#		libncurses5-dev \
-#		libgdbm-dev \
-#		libnss3-dev \
-#		libssl-dev \
-#		libreadline-dev \
-#		libffi-dev \
-#		libsqlite3-dev \
-#		wget \
-#		git \
-#		libbz2-dev
-
-
 apt-get update
 TZ=UTC apt-get install --no-install-recommends python3.11 python3.11-dev python3.11-venv -y
 
