9f3e7fc51d2aa02582335d88d6d42196ec1fa5e9
Stefane Fermigier <sf@fermigier.com>
2023-06-05 12:46:53 +0200

    fix docker build

--- docker/install.sh
+++ docker/install.sh
@@ -6,8 +6,7 @@ apt-get update
 TZ=UTC apt-get install --no-install-recommends python3.11 python3.11-dev python3.11-venv -y
 
 python3 -m venv .
-bin/pip install --no-cache-dir -r requirements.txt
-bin/python setup.py develop
+bin/pip install --no-cache-dir .
 bin/pip install --no-cache-dir supervisor
 
 apt-get purge -y --auto-remove python3.11-dev
