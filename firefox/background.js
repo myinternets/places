fc6d869bd1cda4f380a603587500b04269ba7b26
Tarek ZIADE <tarek@ziade.org>
2023-06-10 14:08:25 -0700

    filter by status code

---
+++ firefox/background.js
@@ -0,0 +1,43 @@
+async function postJSON(data) {
+  try {
+    const response = await fetch("http://localhost:8080/index", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    const result = await response.json();
+    console.log("Success:", result);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+function handleMessage(request, sender, sendResponse) {
+  if (request.url in statusCodes) {
+      var code = statusCodes[request.url];
+      if (code < 300) {
+        postJSON(request);
+      }
+  }
+}
+
+browser.runtime.onMessage.addListener(handleMessage);
+
+var statusCodes = {};
+
+browser.webRequest.onCompleted.addListener(
+
+  e => {
+
+    if (e.tabId === -1 || e.type !== 'main_frame') {
+      return;
+    }
+    statusCodes[e.url] = e.statusCode;
+  },
+  { urls: ['<all_urls>'] , types: ["main_frame"]},
+  ['responseHeaders']
+);
+
--- firefox/places.js
+++ firefox/places.js
@@ -8,27 +8,5 @@ if (document.readyState !== 'complete') {
 
 function afterWindowLoaded() {
   var page = '<html>' + document.documentElement.innerHTML + '</html>';
-  const data = { url: window.location.href, text: page};
-  postJSON(data);
+  browser.runtime.sendMessage({ url: window.location.href, text: page});
 }
-
-
-async function postJSON(data) {
-  try {
-    const response = await fetch("http://localhost:8080/index", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-
-    const result = await response.json();
-    console.log("Success:", result);
-  } catch (error) {
-    console.error("Error:", error);
-  }
-}
-
-
-
